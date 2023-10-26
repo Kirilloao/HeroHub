1157d4b4cb4488442597b55ba09589beb230b02b
Kirilloao <kirilltaraturin747@gmail.com>
2023-10-26 19:49:30 +0200

    Initial Commit

---
+++ Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Heroes
+//
+//  Created by Kirill Taraturin on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
