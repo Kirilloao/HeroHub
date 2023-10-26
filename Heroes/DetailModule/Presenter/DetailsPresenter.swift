2fde6419fc45523f3ac9c67b9b117a1bec378fea
Kirilloao <kirilltaraturin747@gmail.com>
2023-10-26 20:21:06 +0200

    create DetailsPresenter

---
+++ Heroes/DetailModule/Presenter/DetailsPresenter.swift
@@ -0,0 +1,36 @@
+//
+//  DetailsPresenter.swift
+//  Heroes
+//
+//  Created by Kirill Taraturin on 26.10.2023.
+//
+
+import Foundation
+
+protocol DetailViewProtocol: AnyObject {
+    func setHero(_ hero: Hero?)
+}
+
+protocol DetailViewPresenterProtocol: AnyObject {
+    init(view: DetailViewProtocol, networkService: NetworkServiceProtocol, hero: Hero?)
+    func setHero()
+}
+
+class DetailPresenter: DetailViewPresenterProtocol {
+    private unowned var view: DetailViewProtocol?
+    let networkService: NetworkServiceProtocol!
+    var hero: Hero?
+    
+    
+    required init(view: DetailViewProtocol, networkService: NetworkServiceProtocol, hero: Hero?) {
+        self.view = view
+        self.networkService = networkService
+        self.hero = hero
+    }
+    
+    func setHero() {
+        self.view?.setHero(hero)
+    }
+    
+  
+}
