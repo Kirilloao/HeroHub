a328369ab949e92e81ce59973adec9ca751df2d2
Kirilloao <kirilltaraturin747@gmail.com>
2023-10-26 20:09:01 +0200

    work with UI
    Create CharacterCell, CharacterListViewController
    Add SnapKit

---
+++ Heroes/CharacterListModule/View/Cell/CharacterCell.swift
@@ -0,0 +1,134 @@
+//
+//  CharacterCell.swift
+//  Heroes
+//
+//  Created by Kirill Taraturin on 26.10.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class CharacterCell: UICollectionViewCell {
+    
+    // MARK: - Static Properties
+    static let reuseID = String(describing: CharacterCell.self)
+    
+    // MARK: - Private UI Properties
+    private lazy var characterImageView: UIImageView = {
+        let charView = UIImageView()
+        charView.layer.cornerRadius = 10
+        charView.clipsToBounds = true
+        return charView
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        var label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private var activityIndicator: UIActivityIndicatorView?
+
+    // MARK: - Private Properties
+    private var imageURL: URL? {
+        didSet {
+            characterImageView.image = nil
+            updateImage()
+        }
+    }
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setViews()
+        activityIndicator = showSpinner(in: characterImageView)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public Methods
+    func configure(with character: Hero?) {
+        nameLabel.text = character?.name ?? "NO DATA"
+        imageURL = URL(string: character?.images.lg ?? "")
+   
+    }
+    
+    // MARK: - Network Methods
+    private func updateImage() {
+        guard let imageURL = imageURL else { return }
+        
+        getImage(from: imageURL) { result in
+            switch result {
+                
+            case .success(let image):
+                if imageURL == self.imageURL {
+                    self.characterImageView.image = image
+                    self.activityIndicator?.stopAnimating()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func getImage(from url: URL, completion: @escaping(Result<UIImage, Error>) -> Void) {
+        if let cachedImage = ImageCacheManager.shared.object(forKey: url.lastPathComponent as NSString) {
+            completion(.success(cachedImage))
+        }
+        
+        NetworkManager().fetchImage(from: url) { result in
+            switch result {
+                
+            case .success(let imageData):
+                guard let uiImage = UIImage(data: imageData) else { return }
+                ImageCacheManager.shared.setObject(uiImage, forKey: url.lastPathComponent as NSString)
+                completion(.success(uiImage))
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    // MARK: - Setup UI methods
+    private func setupConstraints() {
+        nameLabel.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalTo(characterImageView.snp.bottom)
+        }
+        
+        characterImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.bottom.equalTo(nameLabel.snp.top)
+        }
+    }
+    
+    private func setViews() {
+        contentView.addSubview(characterImageView)
+        contentView.addSubview(nameLabel)
+    }
+    
+    private func showSpinner(in view: UIView) -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.color = .systemGray
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        
+        view.addSubview(activityIndicator)
+        activityIndicator.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.centerY.equalTo(view.snp.centerY)
+        }
+        
+        return activityIndicator
+    }
+}
+
--- Heroes/CharacterListModule/View/CharacterListViewController.swift
+++ Heroes/CharacterListModule/View/CharacterListViewController.swift
@@ -14,13 +14,84 @@ protocol CharacterListProtocol: AnyObject {
 }
 
 
-class CharacterListViewController: UIViewController {
+class CharacterListViewController: UICollectionViewController {
 
+    // MARK: - Presenter
+    var presenter: CharacterListPresenter?
+    
+    // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        registerCell()
+        setupLayout()
+        presenter?.getCharacters()
     }
-
-
+    
+    // MARK: - Init
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func setupLayout() {
+        let layout = UICollectionViewFlowLayout()
+        
+        let padding: CGFloat = 30 // Отступы слева и справа
+        let spacing: CGFloat = 30 // Расстояние между ячейками
+        
+        let availableWidth = view.bounds.width - padding * 2 - spacing
+        let itemWidth = availableWidth / 2
+        
+        
+        layout.itemSize = CGSize(width: itemWidth, height: 200)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: padding, bottom: 10, right: padding)
+        
+        layout.minimumLineSpacing = 10 // минимальное расстояние между строками
+        layout.minimumInteritemSpacing = 15 // минимальное расстояние между элементами в строке
+        collectionView.collectionViewLayout = layout
+    }
+    
+    private func registerCell() {
+        collectionView.register(
+            CharacterCell.self,
+            forCellWithReuseIdentifier: CharacterCell.reuseID
+        )
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        presenter?.heroes?.count ?? 0
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: CharacterCell.reuseID, for: indexPath) as? CharacterCell
+        else {
+            return UICollectionViewCell()
+        }
+        
+        let character = presenter?.heroes?[indexPath.row]
+        cell.configure(with: character)
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
 }
 
+// MARK: - CharacterListProtocol
+extension CharacterListViewController: CharacterListProtocol {
+    func success() {
+        collectionView.reloadData()
+    }
+    
+    func failure(error: NetworkError) {
+        print(error)
+    }
+}
